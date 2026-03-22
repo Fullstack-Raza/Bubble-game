2c92d210b4631a77e8224279f017a10463825c9b
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-03-22 14:18:48 +0500

    bubble game

---
+++ js.js
@@ -0,0 +1,67 @@
+const bp = document.querySelector(".pbtm");
+const time = document.querySelector("#time");
+const hit = document.querySelector("#hit");
+const score = document.querySelector("#score");
+const panel = document.querySelector(".panel");
+const fscore = document.querySelector("#fscore");
+const result = document.querySelector(".result");
+const play = document.querySelector("#restart");
+
+function game(){
+     panel.style.display="block"
+        result.style.display="none"
+let t = 10;
+time.textContent = t;
+let scoreval = 0;
+score.textContent = scoreval;
+function hits() {
+  const num = Math.floor(Math.random() * 10 + 1);
+  hit.textContent = num;
+}
+function timer() {
+  var tint = setInterval(function () {
+    t--;
+    time.textContent = t;
+    if (t == 0) {
+        fscore.textContent =scoreval;
+        panel.style.display="none"
+        result.style.display="block"
+      clearInterval(tint);
+
+    }
+  }, 1000);
+}
+
+function makebubble() {
+  for (let i = 0; i < 120; i++) {
+    const num = Math.floor(Math.random() * 10 + 1);
+    const b = document.createElement("nav");
+    // b.add.classList(bubble)
+    b.textContent = num;
+    bp.append(b);
+  }
+}
+makebubble();
+bp.addEventListener("click", function (e) {
+  if (hit.textContent == e.srcElement.textContent) {
+    scoreval+=10;
+    score.textContent = scoreval;
+  } else {
+    scoreval-=10;
+    score.textContent = scoreval;
+  }
+  hits();
+  bp.innerHTML = "";
+  makebubble();
+});
+makebubble();
+timer();
+hits();
+
+}
+
+game();
+
+play.addEventListener("click",function(){
+game();
+})
